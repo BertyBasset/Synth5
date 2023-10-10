4760c0f9ddd5db0b44fa6a066c1f485cc5bc7797
Robin Griffiths <robin.griffiths@loyaltylogistix.com>
2023-10-10 22:32:56 +0100

    Added VCO Properties

--- WpfUi/Modules/VCO.xaml.cs
+++ WpfUi/Modules/VCO.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,16 +15,70 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfUi.Modules
-{
-    /// <summary>
-    /// Interaction logic for VCO.xaml
-    /// </summary>
-    public partial class VCO : UserControl
-    {
-        public VCO()
-        {
-            InitializeComponent();
+namespace WpfUi.Modules;
+public partial class VCO : UserControl {
+    // Need events for Knob Value Changed
+
+
+
+
+    public string Caption {
+        get { return lblCaption.Text; }
+        set { lblCaption.Text = value; }
+    }
+
+    public Brush CaptionBrush {
+        get { return lblCaption.Foreground; }
+        set { 
+            lblCaption.Foreground = value; 
         }
     }
+
+    public Brush BorderColor {
+        get { return brdBorder.BorderBrush; }
+        set { brdBorder.BorderBrush = value; }
+    }
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+    public new Brush BorderBrush {
+        get { return BorderColor; }
+        set { BorderColor = value; }
+    }
+
+    public Thickness BorderWidth{ 
+        get { return brdBorder.BorderThickness; }
+        set  {brdBorder.BorderThickness = value; }
+    }
+
+    public CornerRadius BorderRadius {
+        get { return brdBorder.CornerRadius; }
+        set { brdBorder.CornerRadius = value; }
+    }
+
+    public double Frequency {
+        get { return knbFrequency.Value; }
+        set { knbFrequency.Value = value;}
+    }
+
+    public double Octave {
+        get { return knbOctave.Value; }
+        set { knbOctave.Value = value; }
+    }
+
+    public double Waveform {
+        get { return knbWaveform.Value; }
+        set { knbWaveform.Value = value; }
+    }
+
+    public double PulseWidth {
+        get { return knbPulseWidth.Value; }
+        set { knbPulseWidth.Value = value; }
+    }
+
+
+    public VCO() {
+        InitializeComponent();
+    }
+
+
+
 }
